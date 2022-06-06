776ae12e46fce66798f2e5167444f57f5a123b0d
ujjawal200 <ujjawalthakur2003@gmail.com>
2022-06-06 12:03:12 +0530

    adding ,deleting,change in object

--- module2/object.js
+++ module2/object.js
@@ -36,3 +36,20 @@ console.log(capamerica.sayHi());
 
 //directly call the function without giving it ot the console to avoid "undefined " eror
 capamerica.sayHi();
+
+// add a new value in object
+
+capamerica.Movies = ["avenger" ,"age of ultron"];
+console.log(capamerica);
+
+// deleteing value from a object
+delete capamerica.Movies;
+
+console.log(capamerica);
+
+// change something in an object
+capamerica.address.state = "new york"
+console.log(capamerica);
+
+// second method of accesing aobject
+console.log(capamerica['name']);
