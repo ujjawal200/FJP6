522580229226f243d07e9aeb2d20f04d945c2274
ujjawal200 <ujjawalthakur2003@gmail.com>
2022-06-06 11:13:36 +0530

    object syntax and uses

--- module2/object.js
+++ module2/object.js
@@ -10,3 +10,17 @@ let obujjawl ={
 
 }
 console.log(obujjawl);
+
+let capamerica = {
+    name:"steve",
+    age: 99,
+    freinds:['Thor','Tony','natasha'],
+    address:{
+        city:"queens",
+        state:"bihar"
+    },
+    sayHi:function(){
+        console.log("bihar ke lala.... jiya tu hazar saala")
+    }
+}
+console.log(capamerica);
