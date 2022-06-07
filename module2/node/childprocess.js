6214861c25a9f5a4576e9e33561795919a77239a
ujjawal200 <ujjawalthakur2003@gmail.com>
2022-06-07 08:24:01 +0530

    child process execSync

--- module2/node/childprocess.js
+++ module2/node/childprocess.js
@@ -11,3 +11,16 @@ cp.execFileSync("calc");
 // calling vs code
 
 cp.execSync("code");
+
+// calling another's file output here using childprocess
+
+let content = cp.execSync("node test.js");
+
+console.log("output from test.js file is :"+content);
+
+// execSync store data in binary form as seen belows output
+console.log(content);
+
+// to overcome this problem we add the content with a empty or a filled string
+
+console.log(""+content);
